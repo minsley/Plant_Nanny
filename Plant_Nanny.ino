08700f57296b55b7246ce88888d4171a7bf1d7f5
Matt Insley <insley.matthew@gmail.com>
2014-08-16 02:58:08 -0500

    Time displayed and logged
    Requires sync via serial after reboot. Look into getting a battery
    powered clock module.

--- Plant_Nanny.ino
+++ Plant_Nanny.ino
@@ -1,3 +1,4 @@
+
 /*
 Plant_Nanny
 
@@ -23,6 +24,7 @@ char temp[6];
 char humid[6];
 char light[4];
 char moist[4];
+char time[6];
 
 /***************************************
 Set up the SD
@@ -36,6 +38,7 @@ Set up the SD
 #define sd_cs  4
 
 File diary;
+String record;
 
 /***************************************
 Set up the HT sensor
@@ -78,6 +81,19 @@ int timeSinceMRead;
 #define MSWaitCycles 12
 
 /***************************************
+Set up time keeping
+***************************************/
+
+#include <Time.h>
+#define TIME_MSG_LEN  11   // time sync to PC is HEADER followed by Unix time_t as ten ASCII digits
+#define TIME_HEADER  'T'   // Header tag for serial time sync message
+#define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
+
+// T1262347200  //noon Jan 1 2010
+
+time_t t;
+
+/***************************************
 Main Setup()
 ***************************************/
 
@@ -94,6 +110,7 @@ void setup()
   TFTscreen.text("Humidity (%)",0,40);
   TFTscreen.text("Luminosity",0,80);
   TFTscreen.text("Moisture",0,120);
+  TFTscreen.text("Time", 104, 120);
   TFTscreen.setTextSize(2);
   Serial.println("TFT Screen initialized.");
   
@@ -115,6 +132,14 @@ Main Loop()
 
 void loop()
 {
+  if(Serial.available())
+  {
+    processSyncMessage();
+  }
+  if(timeStatus() == timeNotSet)
+    Serial.println("waiting for sync message");
+  
+  t = now();
   f = dht.readTemperature(true);
   h = dht.readHumidity();
   l = analogRead(LSPIN);
@@ -133,22 +158,36 @@ void loop()
   TFTscreen.text(humid, 0, 50);
   TFTscreen.text(light, 0, 90);
   TFTscreen.text(moist, 0, 130);
+  TFTscreen.text(time, 70, 130);
   
   dtostrf(f,4,2,temp);
   dtostrf(h,4,2,humid);
   String(l).toCharArray(light, 4);
   String(m).toCharArray(moist, 4);
+  if (timeStatus()!= timeNotSet) 
+  {  
+    formatTime().toCharArray(time, 6);
+    record = String(t) + "\t"; 
+  }
+  else 
+  { 
+    String timeString = "00:00";
+    timeString.toCharArray(time, 6);
+    record = "0000000000\t"; 
+  }
   
   TFTscreen.stroke(255,255,255);
   TFTscreen.text(temp, 0, 10);
   TFTscreen.text(humid, 0, 50);
   TFTscreen.text(light, 0, 90);
   TFTscreen.text(moist, 0, 130);
+  TFTscreen.text(time, 70, 130);
   
-  String record = String(temp)  + "\t" +
-             String(humid) + "\t" +
-             String(light) + "\t" +
-             String(moist);
+  record +=
+     String(temp)  + "\t" +
+     String(humid) + "\t" +
+     String(light) + "\t" +
+     String(moist);
   
   Serial.println(record);
   
@@ -165,4 +204,35 @@ void writeToLog(String text)
     diary.println(text);
   }
   diary.close();
+}
+
+void processSyncMessage() {
+  // if time sync available from serial port, update time and return true
+  while(Serial.available() >=  TIME_MSG_LEN ){  // time message consists of header & 10 ASCII digits
+    char c = Serial.read() ;
+    Serial.print(c);  
+    if( c == TIME_HEADER ) {      
+      time_t pctime = 0;
+      for(int i=0; i < TIME_MSG_LEN -1; i++){  
+        c = Serial.read();          
+        if( c >= '0' && c <= '9'){  
+          pctime = (10 * pctime) + (c - '0') ; // convert digits to a number    
+        }
+      }  
+      setTime(pctime);   // Sync Arduino clock to the time received on the serial port
+    }  
+  }
+}
+
+String formatTime()
+{
+  String prettyTime;
+  
+  if(hour() < 10) prettyTime += "0";
+  prettyTime += String(hour()) + ":";
+  
+  if(minute() < 10) prettyTime += "0";
+  prettyTime += String(minute());
+  
+  return prettyTime;
 }
